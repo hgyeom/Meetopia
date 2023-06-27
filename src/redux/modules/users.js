f4add69e2c61153d6d272c06c15ebc68cfd969ff
hgyeom <rrune3@naver.com>
2023-06-27 10:34:22 +0900

    first commit

---
+++ src/redux/modules/users.js
@@ -0,0 +1,17 @@
+const initialState = [
+  {
+    userid: "aaa@aaa.com",
+    nickname: "닉네임",
+    password: "12341234",
+    profileImg: "없",
+  },
+];
+
+const users = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default users;
