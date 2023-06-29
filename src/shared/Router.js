5312292e96abfbb38d8d02b9de099fc3f9597f9c
Songju-Jeong <js0ngwn@gmail.com>
2023-06-29 10:02:28 +0900

    상세 페이지 CRU(작성,읽기,수정) 기능 구현

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -4,6 +4,7 @@ import Home from '../pages/Home';
 import Comments from '../comments/CommentsPage';
 import Detail from '../pages/Detail';
 import DetailEdit from '../pages/DetailEdit';
+import DetailUpdate from '../components/DetailEdit/DetailUpdate';
 // import Layout from './Layout';
 
 const Router = () => {
@@ -14,6 +15,7 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Comments />} />
         <Route path="/detail" element={<DetailEdit />} />
+        <Route path="/detail/update" element={<DetailUpdate />} />
         <Route path="/detail/:id" element={<Detail />} />
       </Routes>
       {/* </Layout> */}
