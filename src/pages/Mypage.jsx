651308aa231ba5e86b6d15990be793361270b400
Lee Jiyoung <lizzie.yyy@gmail.com>
2023-06-30 10:46:50 +0900

    중간저장

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -4,12 +4,12 @@ import { S } from '../components/Mypage/Mypage.styled';
 import Profile from '../components/Mypage/Profile';
 import MyPosts from '../components/Mypage/MyPosts';
 
-import { useEffect } from 'react';
-import { auth } from '../firebase';
-import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
-import { useDispatch, useSelector } from 'react-redux';
-import { addCurrentUser } from '../redux/modules/users';
-import { styled } from 'styled-components';
+// import { useEffect } from 'react';
+// import { auth } from '../firebase';
+// import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { addCurrentUser } from '../redux/modules/users';
+// import { styled } from 'styled-components';
 
 const Mypage = () => {
   const navigate = useNavigate();
@@ -18,22 +18,22 @@ const Mypage = () => {
     navigate('/mypage/edit');
   };
 
-  useEffect(() => {
-    // 1. 로그인하기
-    // 테스트용 임시 로그인상태 만들기
-    let [testEmail, testPassword] = ['meetopia@naver.com', '123456'];
-    const signIn = async (event) => {
-      try {
-        const userCredential = await signInWithEmailAndPassword(auth, testEmail, testPassword);
-        console.log('user with signIn', userCredential.user);
-      } catch (error) {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log('error with signIn', errorCode, errorMessage);
-      }
-    };
-    signIn();
-  }, []);
+  // useEffect(() => {
+  //   // 1. 로그인하기
+  //   // 테스트용 임시 로그인상태 만들기
+  //   let [testEmail, testPassword] = ['meetopia@naver.com', '123456'];
+  //   const signIn = async (event) => {
+  //     try {
+  //       const userCredential = await signInWithEmailAndPassword(auth, testEmail, testPassword);
+  //       console.log('user with signIn', userCredential.user);
+  //     } catch (error) {
+  //       const errorCode = error.code;
+  //       const errorMessage = error.message;
+  //       console.log('error with signIn', errorCode, errorMessage);
+  //     }
+  //   };
+  //   signIn();
+  // }, []);
 
   return (
     <S.MypageContainer>
